8f89128296be7626be8268e984c9595f8e96c80e
jarmourato <joao.armourato@gmail.com>
2021-06-21 12:49:01 +0100

    Add headers builder

--- Sources/Endpoint+Build.swift
+++ Sources/Endpoint+Build.swift
@@ -4,9 +4,21 @@ import Foundation
 
 extension Endpoint {
     
+    /// Add a custom body build closure to the `Endpoint`.
+    /// - Parameter encoder: A custom body encoder used to create the body of the `Endpoint` request.
+    /// - Returns: Returns a copy of self with a custom body encoder.
     public func withBody(encoder: @escaping BodyEncoder) -> Endpoint {
         var new = self
         new.bodyEncoder = encoder
+        return new
+    }
+    
+    /// Add headers to the `Endpoint`
+    /// - Parameter headers: The headers to be added to the request, when this `Endpoint` is called.
+    /// - Returns: Returns a copy of self with the added headers
+    public func withHeaders(_ headers: [String:String]) -> Endpoint {
+        var new = self
+        new.headers = Set(headers.map(HTTPHeader.init))
         return new
     }
 }
