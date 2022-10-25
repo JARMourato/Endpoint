37e69a816593c4eadcaa7f282d3f12dba980b1b8
JARMourato <joao.armourato@gmail.com>
2022-10-25 01:24:55 +0100

    add http options

--- Sources/Endpoint+HTTP.swift
+++ Sources/Endpoint+HTTP.swift
@@ -5,11 +5,14 @@ import Foundation
 /// Type representing HTTP methods.
 /// See https://tools.ietf.org/html/rfc7231#section-4.3
 public struct HTTPMethod: Hashable {
+    public static let connect = HTTPMethod(rawValue: "CONNECT")
     public static let delete = HTTPMethod(rawValue: "DELETE")
     public static let get = HTTPMethod(rawValue: "GET")
+    public static let options = HTTPMethod(rawValue: "OPTIONS")
     public static let patch = HTTPMethod(rawValue: "PATCH")
     public static let post = HTTPMethod(rawValue: "POST")
     public static let put = HTTPMethod(rawValue: "PUT")
+    public static let trace = HTTPMethod(rawValue: "TRACE")
 
     public let rawValue: String
 }
